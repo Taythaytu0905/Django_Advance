b68918f960b1a4f2384d85358dcfc72834a6f608
Do Nguyen <ntdo.13cdt1@gmail.com>
2020-12-27 21:00:51 +0700

    add image file on models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,6 @@
+import os
+import uuid
+
 from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
@@ -6,6 +9,13 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from .manager import CustomUserManager
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image"""
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return os.path.join('uploads/recipe/', filename)
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
@@ -60,6 +70,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     ingredients = models.ManyToManyField('Ingredient')
     tags = models.ManyToManyField('Tag')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
